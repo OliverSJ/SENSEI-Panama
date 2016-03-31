aa4c0d2cd5070e1948531752c66281bdb0e3df3b
Oliver San Juan <osanju2@gmail.com>
2016-03-31 11:32:08 -0500

    Edited planeview.py to read from csv file and display points

--- planeview.py
+++ planeview.py
@@ -9,25 +9,17 @@
 	C: 624030.0137255481 	<--upper left easting
 	F; 1015207.0834458455 	<--- upper left northing
 
-
-	627407.233983497,1012918.6367208124
-
-
 	To find the ratio:
 		d/totald * 100
 
-
-	We need to know the bottom right hand corner in terms of UTM
-
-	Dimensions of the picture:
+	Dimension of the picture:
 		32064 x 30780
-
-	-Is it -0.18 or +0.18?
 
 """
 
 from omega import *
 from cyclops import *
+import csv
 
 #Constants
 upperLeftEasting = 624030.0137255481		#Provided by jgw file
@@ -53,21 +45,46 @@ totalDistanceX = lowerRightEasting - upperLeftEasting
 totalDistanceY = lowerRightNorthing - upperLeftNorthing
 
 #testing
-testLeftEasting = 627407.233983497
-testLeftNorthing = 1012918.6367208124
+f = open('test.csv')
+csv_f = csv.reader(f)
 
-sphere = SphereShape.create(0.36, 3)
+i = csv_f.next()
+rest = [row for row in csv_f]
 
-#Calculate the distance
-distanceX = upperLeftEasting - testLeftEasting
-distanceY = upperLeftNorthing- testLeftNorthing
+for i in rest:
+	count =0
+	#Grab each row
+	for j in rest:
+		testLeftEasting = float(j[2])
+		testLeftNorthing = float(j[3])
+		sphere = SphereShape.create(0.36, 3)
 
-#Convert the distance into a ratio to be graphed
-adjustedX = (distanceX/totalDistanceX) * 100
-adjustedY = (distanceY/totalDistanceY) * 100
+		#Calculate the distance
+		distanceX = upperLeftEasting - testLeftEasting
+		distanceY = upperLeftNorthing- testLeftNorthing
 
-sphere.setPosition(Vector3(adjustedX, adjustedY, 0))
-sphere.setEffect("colored -d red")
+		#Convert the distance into a ratio to be graphed
+		adjustedX = (distanceX/totalDistanceX) * 100
+		adjustedY = (distanceY/totalDistanceY) * 100
+
+		sphere.setPosition(Vector3(adjustedX, adjustedY, 0))
+		sphere.setEffect("colored -d red")
+
+# testLeftEasting = 627407.233983497
+# testLeftNorthing = 1012918.6367208124
+
+# sphere = SphereShape.create(0.36, 3)
+
+# #Calculate the distance
+# distanceX = upperLeftEasting - testLeftEasting
+# distanceY = upperLeftNorthing- testLeftNorthing
+
+# #Convert the distance into a ratio to be graphed
+# adjustedX = (distanceX/totalDistanceX) * 100
+# adjustedY = (distanceY/totalDistanceY) * 100
+
+# sphere.setPosition(Vector3(adjustedX, adjustedY, 0))
+# sphere.setEffect("colored -d red")
 
 
 #Example code to color the monkeys' positions
