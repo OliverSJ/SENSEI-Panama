8254de1384809079dafd56726d73380c6d4c5bb9
Jillian Aurisano <jillian.aurisano@gmail.com>
2016-03-31 16:15:23 -0500

    i think positons are correct now but had to do a weird hack by texturing the spheres because the system can not find cyclops shaders.

--- planeview.py
+++ planeview.py
@@ -21,6 +21,17 @@ from omega import *
 from cyclops import *
 import csv
 
+print "hello!"
+
+#setup lights
+scene = getSceneManager()
+light1 = Light.create()
+light1Color = Color("#ffffff")
+light1.setColor(light1Color)
+light1.setAmbient(light1Color)
+light1.setEnabled(True)
+
+
 #Constants
 upperLeftEasting = 624030.0137255481		#Provided by jgw file
 upperLeftNorthing = 1015207.0834458455		#Provided by jgw file
@@ -29,20 +40,33 @@ resHeight = 30780
 pixelSize = 0.18
 
 #Create plane with specified width and height
-plane = PlaneShape.create(100, 100)
+plane = PlaneShape.create(100, 100) 
 
 #Set the upper corner
-plane.setPosition(Vector3(0, 0, -3))
+plane.setPosition(Vector3(50, -50, -3))
 
-plane.setEffect("textured -v emissive -d 50Island.png")
+#testing where corners are
+upperCornerSphere = SphereShape.create(1.0, 3)
+upperCornerSphere.setPosition(0,0,-3)
+
+lowerCornerSphere = SphereShape.create(1.0, 3)
+lowerCornerSphere.setPosition(100,-100,-3)
+
+
+print "---map loaded..."
+
+plane.setEffect("textured -d 50Island.png -v emissive")
 
 #Calculate the bottom right UTM coordinates for our file
 lowerRightEasting = (pixelSize * resWidth) + upperLeftEasting
-lowerRightNorthing = (pixelSize * resHeight) + upperLeftEasting
+lowerRightNorthing = upperLeftNorthing - (pixelSize * resHeight) 
 
 #Calculate the total distances
 totalDistanceX = lowerRightEasting - upperLeftEasting
 totalDistanceY = lowerRightNorthing - upperLeftNorthing
+
+
+print "---open file..."
 
 #testing
 f = open('test.csv')
@@ -51,22 +75,39 @@ csv_f = csv.reader(f)
 i = csv_f.next()
 rest = [row for row in csv_f]
 
+fixedZ = -2
+
 for i in rest:
 	if(i[2] and i[3]):
 		testLeftEasting = float(i[2])
 		testLeftNorthing = float(i[3])
-		sphere = SphereShape.create(0.36, 3)
+		sphere = SphereShape.create(1.0, 3)
 
 		#Calculate the distance
-		distanceX = upperLeftEasting - testLeftEasting
-		distanceY = upperLeftNorthing- testLeftNorthing
+		distanceX = testLeftEasting - upperLeftEasting
+		distanceY = upperLeftNorthing - testLeftNorthing
 
 		#Convert the distance into a ratio to be graphed
 		adjustedX = (distanceX/totalDistanceX) * 100
 		adjustedY = (distanceY/totalDistanceY) * 100
 
-		sphere.setPosition(Vector3(adjustedX, adjustedY, 0))
-		sphere.setEffect("colored -d red")
+		#color by time
+		time = i[4]
+		timeTokens1 = time.split(" ")
+		timeTokens2 = timeTokens1[1].split(":")
+		print timeTokens2[0]
+		
+		timeCat = int(int(timeTokens2[0])/4)
+		effectStr = "textured -v emissive -d %d.png" % timeCat #hack... 
+
+		sphere.setPosition(Vector3(adjustedX, adjustedY, fixedZ))
+		#sphere.setEffect("colored -d red -v emissive")
+		sphere.setEffect(effectStr)
+		print "creating sphere at ( %.5f, %.5f, %.5f )" % (adjustedX, adjustedY, fixedZ)
+
+
+
+print "done"
 
 # testLeftEasting = 627407.233983497
 # testLeftNorthing = 1012918.6367208124
